09463d69fd056dc6345bc76ce3107d8b1efcfd4c
Nolwenn PENNEC CHARLES <nolwenn.pennec@hotmail.com>
2024-12-18 12:44:50 +0100

    mise en place de l'arborescence et du début des routes

---
+++ frontend/my-react-app/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Layout} from './Layout'
+export { default as Home} from './Home'
+export { default as FicheLogement} from './FicheLogement'
+export { default as APropos} from './APropos'
